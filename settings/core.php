fdd7015e48b36064650b4b05c880fe30e95fafa8
annmarieakuba <aaemensah.@gmail.com>
2025-09-25 17:51:04 +0000

    Update lab 3 part 1

--- settings/core.php
+++ settings/core.php
@@ -1,18 +1,21 @@
 <?php
 //start session
-session_start(); 
+if (session_status() === PHP_SESSION_NONE) {
+session_start();
+}
 
 //for header redirection
 ob_start();
 
 //funtion to check for login
-
-
-//function to get user ID
-
+function is_logged_in() {
+return isset($_SESSION['customer_id']) && !empty($_SESSION['customer_id']);
+}
 
 //function to check for role (admin, customer, etc)
-
+function is_admin() {
+return isset($_SESSION['user_role']) && (int)$_SESSION['user_role'] === 1;
+}
 
 
 ?>
